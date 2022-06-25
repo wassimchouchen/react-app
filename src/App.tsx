83a8bc045ed37686282d9a084371385b9b37f2a6
Nie-Mand <chill.out.x19@gmail.com>
2022-06-25 15:26:11 +0100

    setup prettier, typescript and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="w-screen h-screen grid place-content-center bg-[#2b2b28]">
+      <div className="p-20 w-96 h-96 rounded-full border-[20px] grid place-content-center border-[#4b4b49]">
+        <img className="w-80" src="/lambda.svg" alt="Lambda" />
+      </div>
+    </div>
+  )
+}
+
+export default App
